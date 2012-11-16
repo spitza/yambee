830c5a3f71b219c0c5903887774fb54c3c000b2c
Phil Spitzer <thespitzer@gmail.com>
2012-11-15 16:43:45 -0800

    trying again

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <html>
+
+<body>
+    
 <?php
-
-
-
 $name="Phil Spitzer";
 echo $name;
-
 ?>
 
+<form action="testyambee.php" method="post">
+Do you know how to spell this name? <input type="text" name="fname" />
+<input type="submit" />
+</form>
+
+</body>
 
 </html>
--- testyambee.php
+++ testyambee.php
@@ -1,3 +1,8 @@
-<?php
-echo "sddfsfsdfdsfs";
-?>
+<html>
+<body>
+
+Welcome <?php echo $_POST["fname"]; ?>!<br />
+You are <?php echo $_POST["age"]; ?> years old.
+
+</body>
+</html>
