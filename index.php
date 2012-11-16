2c0373f0bd72e512ce59b2b187171b23bcf3a8ec
Phil Spitzer <thespitzer@gmail.com>
2012-11-15 16:33:42 -0800

    ya

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <html>
 <?php
-<head>
-<title>yambee test</title>
-//<link REL="SHORTCUT ICON" HREF="http://www.dontgetdistracted.com/dontgetdistracted.ico">
-<link rel="stylesheet" type="text/css" href="" />
-<script src="yambee.js" type="text/javascript"></script>
 
-</head>
 
 
 $name="Phil Spitzer";
