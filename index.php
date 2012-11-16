ec2f2e92f18b8d4aa81df28f4a6c551962e43179
Phil Spitzer <thespitzer@gmail.com>
2012-11-15 16:30:57 -0800

    try 5

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<html>
 <?php
-echo "trying out an index file";
+<head>
+<title>yambee test</title>
+//<link REL="SHORTCUT ICON" HREF="http://www.dontgetdistracted.com/dontgetdistracted.ico">
+<link rel="stylesheet" type="text/css" href="" />
+<script src="yambee.js" type="text/javascript"></script>
+
+</head>
+
+
+$name="Phil Spitzer";
+echo $name;
+
 ?>
+
+
+</html>
